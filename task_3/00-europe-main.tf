03ec1805c9bce0421ea71ea9b736420717b5a51e
Guy Carlos Villette <carlosvillette@gmail.com>
2024-05-20 20:48:39 -0400

    got startup script to work

--- task_3/00-europe-main.tf
+++ task_3/00-europe-main.tf
@@ -40,7 +40,10 @@ resource "google_compute_instance" "european_instance" {
     }
   }
 
-  metadata_startup_script = file("${path.module}/startup-script.sh")
+  #metadata_startup_script = file("${path.module}/startup-script.sh")
+  metadata = {
+    startup-script = "#Thanks to Remo\n#!/bin/bash\n# Update and install Apache2\napt update\napt install -y apache2\n\n# Start and enable Apache2\nsystemctl start apache2\nsystemctl enable apache2\n\n# GCP Metadata server base URL and header\nMETADATA_URL=\"http://metadata.google.internal/computeMetadata/v1\"\nMETADATA_FLAVOR_HEADER=\"Metadata-Flavor: Google\"\n\n# Use curl to fetch instance metadata\nlocal_ipv4=$(curl -H \"$${METADATA_FLAVOR_HEADER}\" -s \"$${METADATA_URL}/instance/network-interfaces/0/ip\")\nzone=$(curl -H \"$${METADATA_FLAVOR_HEADER}\" -s \"$${METADATA_URL}/instance/zone\")\nproject_id=$(curl -H \"$${METADATA_FLAVOR_HEADER}\" -s \"$${METADATA_URL}/project/project-id\")\nnetwork_tags=$(curl -H \"$${METADATA_FLAVOR_HEADER}\" -s \"$${METADATA_URL}/instance/tags\")\n\n# Create a simple HTML page and include instance details\ncat <<EOF > /var/www/html/index.html\n<html><body>\n<h2>Welcome to your custom website.</h2>\n<h3>Created with a direct input startup script!</h3>\n<p><b>Instance Name:</b> $(hostname -f)</p>\n<p><b>Instance Private IP Address: </b> $local_ipv4</p>\n<p><b>Zone: </b> $zone</p>\n<p><b>Project ID:</b> $project_id</p>\n<p><b>Network Tags:</b> $network_tags</p>\n</body></html>\nEOF"
+  }
 }
 
 # Firewall rules to allow only internal traffic on port 80
@@ -49,7 +52,11 @@ resource "google_compute_firewall" "eu_http" {
   network = google_compute_network.european_network.id
   allow {
     protocol = "tcp"
-    ports    = ["80","22","3389"]
+    ports    = ["22","3389"]
+  }
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
   }
   allow {
     protocol = "icmp"
--- task_3/01-america-main.tf
+++ task_3/01-america-main.tf
@@ -99,7 +99,7 @@ resource "google_compute_instance" "us-instance1" {
     startup-script = "#Thanks to Remo\n#!/bin/bash\n# Update and install Apache2\napt update\napt install -y apache2\n\n# Start and enable Apache2\nsystemctl start apache2\nsystemctl enable apache2\n\n# GCP Metadata server base URL and header\nMETADATA_URL=\"http://metadata.google.internal/computeMetadata/v1\"\nMETADATA_FLAVOR_HEADER=\"Metadata-Flavor: Google\"\n\n# Use curl to fetch instance metadata\nlocal_ipv4=$(curl -H \"$${METADATA_FLAVOR_HEADER}\" -s \"$${METADATA_URL}/instance/network-interfaces/0/ip\")\nzone=$(curl -H \"$${METADATA_FLAVOR_HEADER}\" -s \"$${METADATA_URL}/instance/zone\")\nproject_id=$(curl -H \"$${METADATA_FLAVOR_HEADER}\" -s \"$${METADATA_URL}/project/project-id\")\nnetwork_tags=$(curl -H \"$${METADATA_FLAVOR_HEADER}\" -s \"$${METADATA_URL}/instance/tags\")\n\n# Create a simple HTML page and include instance details\ncat <<EOF > /var/www/html/index.html\n<html><body>\n<h2>Welcome to your custom website.</h2>\n<h3>Created with a direct input startup script!</h3>\n<p><b>Instance Name:</b> $(hostname -f)</p>\n<p><b>Instance Private IP Address: </b> $local_ipv4</p>\n<p><b>Zone: </b> $zone</p>\n<p><b>Project ID:</b> $project_id</p>\n<p><b>Network Tags:</b> $network_tags</p>\n</body></html>\nEOF"
   }
 
-  #tags = ["us1", "vpn", "iam-ssh-allowed"]
+  tags = ["us1"]
 }
 
 
@@ -125,7 +125,7 @@ resource "google_compute_firewall" "am_http" {
   source_ranges = ["0.0.0.0/0",google_compute_subnetwork.european_subnet.ip_cidr_range]
   #target_tags = ["us-instance2","iap-ssh-allowed"]
 }
-/*
+
 resource "google_compute_instance" "us-instance2" {
   depends_on = [ google_compute_subnetwork.american_subnet2 ]
   name         = "us-instance2"
@@ -138,13 +138,16 @@ resource "google_compute_instance" "us-instance2" {
   }
 
   network_interface {
-    network = google_compute_network.american_network2.id
+    network = google_compute_network.american_network1.id
     subnetwork = google_compute_subnetwork.american_subnet2.id
     access_config {
       // Ephemeral IP, no external IP
     }
   }
 
-  tags = ["us2", "vpn", "iam-ssh-allowed"]
+  metadata = {
+    startup-script = "#Thanks to Remo\n#!/bin/bash\n# Update and install Apache2\napt update\napt install -y apache2\n\n# Start and enable Apache2\nsystemctl start apache2\nsystemctl enable apache2\n\n# GCP Metadata server base URL and header\nMETADATA_URL=\"http://metadata.google.internal/computeMetadata/v1\"\nMETADATA_FLAVOR_HEADER=\"Metadata-Flavor: Google\"\n\n# Use curl to fetch instance metadata\nlocal_ipv4=$(curl -H \"$${METADATA_FLAVOR_HEADER}\" -s \"$${METADATA_URL}/instance/network-interfaces/0/ip\")\nzone=$(curl -H \"$${METADATA_FLAVOR_HEADER}\" -s \"$${METADATA_URL}/instance/zone\")\nproject_id=$(curl -H \"$${METADATA_FLAVOR_HEADER}\" -s \"$${METADATA_URL}/project/project-id\")\nnetwork_tags=$(curl -H \"$${METADATA_FLAVOR_HEADER}\" -s \"$${METADATA_URL}/instance/tags\")\n\n# Create a simple HTML page and include instance details\ncat <<EOF > /var/www/html/index.html\n<html><body>\n<h2>Welcome to your custom website.</h2>\n<h3>Created with a direct input startup script!</h3>\n<p><b>Instance Name:</b> $(hostname -f)</p>\n<p><b>Instance Private IP Address: </b> $local_ipv4</p>\n<p><b>Zone: </b> $zone</p>\n<p><b>Project ID:</b> $project_id</p>\n<p><b>Network Tags:</b> $network_tags</p>\n</body></html>\nEOF"
+  }
+
+  tags = ["us2"]
 }
-*/
